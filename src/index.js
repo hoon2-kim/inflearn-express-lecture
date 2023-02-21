6c9c3a69c9ca703bd916ad4faf8b076dcc3bf3bd
hoon2-kim <gooaba1204@gmail.com>
2023-02-22 00:37:35 +0900

    cors 수정2

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ import { jwtAuth } from "./middleware";
     await database.$connect();
 
     // middleware
-    app.use(cors());
+    app.use(
+        cors({
+            origin: "*",
+        })
+    );
     app.use(helmet());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
